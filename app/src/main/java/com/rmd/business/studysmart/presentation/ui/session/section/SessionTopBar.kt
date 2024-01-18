643b9dc57b7d96fe26520cede4d023f15b8036be
diallo <72153331+mamadouramadan723@users.noreply.github.com>
2024-01-18 19:54:11 +0100

    3. Session Screen Design

---
+++ app/src/main/java/com/rmd/business/studysmart/presentation/ui/session/section/SessionTopBar.kt
@@ -0,0 +1,34 @@
+package com.rmd.business.studysmart.presentation.ui.session.section
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun SessionTopBar(
+        onBackButtonClick: () -> Unit
+) {
+    TopAppBar(
+        navigationIcon = {
+            IconButton(onClick = onBackButtonClick) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "Navigate to Back Screen"
+                )
+            }
+        },
+        title = {
+            Text(
+                text = "Study Sessions",
+                style = MaterialTheme.typography.headlineSmall
+            )
+        }
+    )
+}
---
+++ app/src/main/java/com/rmd/business/studysmart/presentation/ui/session/section/TimerSection.kt
@@ -0,0 +1,37 @@
+package com.rmd.business.studysmart.presentation.ui.session.section
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+private fun TimerSection(
+        modifier: Modifier
+) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .size(250.dp)
+                .border(
+                    5.dp,
+                    MaterialTheme.colorScheme.surfaceVariant,
+                    CircleShape
+                )
+        )
+        Text(
+            text = "00:05:32",
+            style = MaterialTheme.typography.titleLarge.copy(fontSize = 45.sp)
+        )
+    }
+}
