6d93e9405abaad306174b2a61cf4910e85745a52
diallo <72153331+mamadouramadan723@users.noreply.github.com>
2024-01-18 19:43:34 +0100

    3. Session Screen Design

---
+++ app/src/main/java/com/rmd/business/studysmart/presentation/ui/session/SessionScreen.kt
@@ -0,0 +1,24 @@
+package com.rmd.business.studysmart.presentation.ui.session
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun SessionScreen(
+        onBackButtonClick: () -> Unit
+) {
+
+    val scope = rememberCoroutineScope()
+    val sheetState = rememberModalBottomSheetState()
+    var isBottomSheetOpen by remember { mutableStateOf(false) }
+
+    var isDeleteDialogOpen by rememberSaveable { mutableStateOf(false) }
+}
