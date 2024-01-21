68e4fe9beef11dc08fe10f0e01745eb7e3e17758
diallo <72153331+mamadouramadan723@users.noreply.github.com>
2024-01-21 19:58:51 +0100

    5. Session Screen Design

--- app/src/main/java/com/rmd/business/studysmart/presentation/ui/session/SessionScreen.kt
+++ app/src/main/java/com/rmd/business/studysmart/presentation/ui/session/SessionScreen.kt
@@ -9,6 +9,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import com.rmd.business.studysmart.data.datasource.local.subjects
+import com.rmd.business.studysmart.presentation.component.DialogDelete
+import com.rmd.business.studysmart.presentation.component.SubjectListBottomSheet
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -21,4 +25,30 @@ fun SessionScreen(
     var isBottomSheetOpen by remember { mutableStateOf(false) }
 
     var isDeleteDialogOpen by rememberSaveable { mutableStateOf(false) }
+
+
+    SubjectListBottomSheet(
+        sheetState = sheetState,
+        isOpen = isBottomSheetOpen,
+        subjects = subjects,
+        onDismissRequest = { isBottomSheetOpen = false },
+        onSubjectClicked = {
+            scope.launch { sheetState.hide() }
+                .invokeOnCompletion {
+                    if (!sheetState.isVisible) isBottomSheetOpen = false
+                }
+        }
+    )
+
+    DialogDelete(
+        isOpen = isDeleteDialogOpen,
+        title = "Delete Session?",
+        bodyText = "Are you sure, you want to delete this session? " +
+                "This action can not be undone.",
+        onDismissRequest = { isDeleteDialogOpen = false },
+        onConfirmButtonClick = {
+            isDeleteDialogOpen = false
+        }
+    )
+
 }
