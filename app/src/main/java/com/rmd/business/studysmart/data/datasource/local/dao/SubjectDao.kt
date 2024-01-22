863a48e93831997839af66ee86dc9c5ad566fe5e
diallo <72153331+mamadouramadan723@users.noreply.github.com>
2024-01-22 20:54:23 +0100

    7. Room Setup (Dao)

---
+++ app/src/main/java/com/rmd/business/studysmart/data/datasource/local/dao/SubjectDao.kt
@@ -0,0 +1,29 @@
+package com.rmd.business.studysmart.data.datasource.local.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import com.rmd.business.studysmart.domain.model.Subject
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SubjectDao {
+
+    @Upsert
+    suspend fun upsertSubject(subject: Subject)
+
+    @Query("SELECT COUNT(*) FROM SUBJECT")
+    fun getTotalSubjectCount(): Flow<Int>
+
+    @Query("SELECT SUM(goalHours) FROM SUBJECT")
+    fun getTotalGoalHours(): Flow<Float>
+
+    @Query("SELECT * FROM Subject WHERE subjectId = :subjectId")
+    suspend fun getSubjectById(subjectId: Int): Subject?
+
+    @Query("DELETE FROM Subject WHERE subjectId = :subjectId")
+    suspend fun deleteSubject(subjectId: Int)
+
+    @Query("SELECT * FROM Subject")
+    fun getAllSubjects(): Flow<List<Subject>>
+}
