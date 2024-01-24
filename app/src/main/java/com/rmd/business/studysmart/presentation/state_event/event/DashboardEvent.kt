69a45ab8bffb1d616f4cea9d031a80b1eb0bf421
diallo <72153331+mamadouramadan723@users.noreply.github.com>
2024-01-24 22:12:19 +0100

    11. State and Event

---
+++ app/src/main/java/com/rmd/business/studysmart/presentation/state_event/event/DashboardEvent.kt
@@ -0,0 +1,15 @@
+package com.rmd.business.studysmart.presentation.state_event.event
+
+import androidx.compose.ui.graphics.Color
+import com.rmd.business.studysmart.domain.model.Session
+import com.rmd.business.studysmart.domain.model.Task
+
+sealed class DashboardEvent {
+    data object SaveSubject : DashboardEvent()
+    data object DeleteSession : DashboardEvent()
+    data class OnDeleteSessionButtonClick(val session: Session) : DashboardEvent()
+    data class OnTaskIsCompleteChange(val task: Task) : DashboardEvent()
+    data class OnSubjectCardColorChange(val colors: List<Color>) : DashboardEvent()
+    data class OnSubjectNameChange(val name: String) : DashboardEvent()
+    data class OnGoalStudyHoursChange(val hours: String) : DashboardEvent()
+}
