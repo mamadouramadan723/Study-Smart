f7515eb8cfd75eb71814b07582c9930cb5801736
diallo <72153331+mamadouramadan723@users.noreply.github.com>
2024-01-25 21:10:37 +0100

    10. ViewModel

--- app/src/main/java/com/rmd/business/studysmart/presentation/viewmodel/DashboardViewModel.kt
+++ app/src/main/java/com/rmd/business/studysmart/presentation/viewmodel/DashboardViewModel.kt
@@ -1,11 +1,15 @@
 package com.rmd.business.studysmart.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.rmd.business.studysmart.domain.repository.SessionRepository
 import com.rmd.business.studysmart.domain.repository.SubjectRepository
+import com.rmd.business.studysmart.domain.repository.TaskRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class DashboardViewModel @Inject constructor(
-        private val subjectRepository: SubjectRepository
+        private val subjectRepository: SubjectRepository,
+        private val sessionRepository: SessionRepository,
+        private val taskRepository: TaskRepository
 ) : ViewModel()
