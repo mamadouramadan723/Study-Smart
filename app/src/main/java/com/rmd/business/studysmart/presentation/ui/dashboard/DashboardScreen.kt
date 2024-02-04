cd2871bf5de3a5b223df8da241fcda555cf073f7
diallo <72153331+mamadouramadan723@users.noreply.github.com>
2024-02-04 20:17:06 +0100

    11. Event, State, ViewModel

--- app/src/main/java/com/rmd/business/studysmart/presentation/ui/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/rmd/business/studysmart/presentation/ui/dashboard/DashboardScreen.kt
@@ -103,7 +103,6 @@ fun DashboardScreen(
                 tasks = tasks,
                 onCheckBoxClick = { onEvent(DashboardEvent.OnTaskIsCompleteChange(it)) },
                 onTaskCardClick = onTaskCardClick
-
             )
             item {
                 Spacer(modifier = Modifier.height(20.dp))
