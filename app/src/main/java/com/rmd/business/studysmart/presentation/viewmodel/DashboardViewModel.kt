5307da3ce102af1a2c6bf200a1689dfd636ba432
diallo <72153331+mamadouramadan723@users.noreply.github.com>
2024-02-04 19:53:00 +0100

    11. Event, State, ViewModel

--- app/src/main/java/com/rmd/business/studysmart/presentation/viewmodel/DashboardViewModel.kt
+++ app/src/main/java/com/rmd/business/studysmart/presentation/viewmodel/DashboardViewModel.kt
@@ -1,16 +1,42 @@
 package com.rmd.business.studysmart.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.rmd.business.studysmart.domain.repository.SessionRepository
 import com.rmd.business.studysmart.domain.repository.SubjectRepository
+import com.rmd.business.studysmart.domain.repository.TaskRepository
 import com.rmd.business.studysmart.presentation.state.DashboardState
+import com.rmd.business.studysmart.presentation.utils.toHours
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class DashboardViewModel @Inject constructor(
-        private val subjectRepository: SubjectRepository
-) :
-    ViewModel() {
+        private val subjectRepository: SubjectRepository,
+        private val sessionRepository: SessionRepository,
+        private val taskRepository: TaskRepository
+) : ViewModel() {
     private val _state = MutableStateFlow(DashboardState())
+    val state = combine(
+        _state,
+        subjectRepository.getTotalSubjectCount(),
+        subjectRepository.getTotalGoalHours(),
+        subjectRepository.getAllSubjects(),
+        sessionRepository.getTotalSessionsDuration()
+    ) { state, subjectCount, goalHours, subjects, totalSessionDuration ->
+        state.copy(
+            subjects = subjects,
+            totalGoalStudyHours = goalHours,
+            totalSubjectCount = subjectCount,
+            totalStudiedHours = totalSessionDuration.toHours()
+        )
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5000),
+        initialValue = DashboardState()
+    )
 }
