6c45588247a471e1cda8386027572a86f4382413
diallo <72153331+mamadouramadan723@users.noreply.github.com>
2024-02-04 19:00:06 +0100

    11. Event, State, ViewModel

--- app/src/main/java/com/rmd/business/studysmart/data/datasource/local/db/ColorListConverter.kt
+++ app/src/main/java/com/rmd/business/studysmart/data/datasource/local/db/ColorListConverter.kt
@@ -1,23 +1,17 @@
 package com.rmd.business.studysmart.data.datasource.local.db
 
-import androidx.compose.ui.graphics.Color
 import androidx.room.TypeConverter
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 
 class ColorListConverter {
 
     @TypeConverter
-    fun fromColorList(colorList: List<Color>): String {
-        return Gson().toJson(colorList)
+    fun fromColorList(colorList: List<Int>): String {
+        return colorList.joinToString(",") { it.toString() }
     }
 
     @TypeConverter
-    fun toColorList(colorString: String): List<Color> {
-        val listType = object : TypeToken<List<Color>>() {}.type
-        return Gson().fromJson(
-            colorString,
-            listType
-        )
+    fun toColorList(colorListString: String): List<Int> {
+        return colorListString.split(",")
+            .map { it.toInt() }
     }
 }
